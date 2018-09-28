632706415f8df28930893f98828c696773bee9a8
BarefootBKK <BarefootBKK@163.com>
2018-09-28 11:58:39 +0800

    底部导航栏

--- app/src/main/java/com/example/gymclubapp/activity/MainActivity.java
+++ app/src/main/java/com/example/gymclubapp/activity/MainActivity.java
@@ -1,10 +1,35 @@
 package com.example.gymclubapp.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+import android.widget.TextView;
 import com.example.gymclubapp.R;
 import com.example.gymclubapp.controller.ActivityController;
 
 public class MainActivity extends BaseActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +37,10 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
 
         ActivityController.finishActivity(ActivityController.activity_signIn);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
